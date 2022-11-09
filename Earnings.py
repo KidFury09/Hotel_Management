6d0c97103039b701cf1013961a2a1221080621d7
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-09 13:37:23 +0400

    Update Earnings.py

--- Earnings.py
+++ Earnings.py
@@ -1,25 +1,19 @@
-from matplotlib.pyplot import pie 
+import matplotlib.pyplot as mpl
 
-earnings = 0 #import this from the sql file
-rooms = [] #better to import each sperately, else a list
-salaries = 0 #sum of the worker salaries
-c1 = 0
-c2 = 0
-c3 = 0
-c4 = 0
+c1 = 20
+c2 = 20
+c3 = 20
+c4 = 20
 
 print(f"Number of Standard Room: {c1}")
 print(f"Number of Superior Room: {c2}")
 print(f"Number of Executive Rooms: {c3}")
 print(f"Number of Presidential: {c4}")
-print()
-print(f"Total income: {earnings}")
-print()
 
-size = [electric_cost, water_cost, salarier, maintanance]
-labels = ["Electricity", "Water", "Salary", "Maintanace"]
+
 colors = ["red", "orange", "yellow", "lightskyblue"]
 
-pie(size, labels = labels, colors = colors)
 
-pie([c1, c2, c3, c4], ["Standard Rooms", "Superior Rooms", "Executive Rooms", "Presidential Suites"], colors = colors)
+mpl.pie([c1, c2, c3, c4], labels = ["Standard Rooms", "Superior Rooms", "Executive Rooms", "Presidential Suites"], colors = colors)
+
+mpl.show()
