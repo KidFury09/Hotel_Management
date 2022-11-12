16302b69d3c42e8f50b64c1b949f875065adc880
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-12 08:38:41 +0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,4 +1,6 @@
-ids = {"Keshav" : ["Password","Manger"], "Hayden" : ["password", "Front_Desk"]}
+from hmng import manager
+
+ids = {"Keshav" : ["Password","Manager"], "Hayden" : ["password", "Front_Desk"]}
 #Assume Keshav is the manager and Hayden is the front desk clerk
 
 print("Please login")
@@ -10,8 +12,11 @@ print()
 while True:
     if User in ids:
         psw = ids[User]
-        if Pass == psw[0]:
-            break
+        if Pass == psw[0] and psw[1] == "Manager":
+            manager()
+        elif Pass == psw[0] and psw[1] == "Front_Desk":
+            pass
+            #check for which user; manager or front desk
             # Manager or clerk interface
         else:
             print("Password is incorrect. Please try again")
