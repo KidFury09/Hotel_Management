1fde89923d0dd4a47469197afd0cf07c45587687
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-12 08:30:42 +0400

    Update Earnings.py

--- Earnings.py
+++ Earnings.py
@@ -1,19 +1,34 @@
 import matplotlib.pyplot as mpl
+import mysql.connector as mql
 
-c1 = 20
-c2 = 20
-c3 = 20
-c4 = 20
+def earning():
+    obj = mql.connect(host = "localhost", user = "root", passwd = "hayden", database = "hotel")
 
-print(f"Number of Standard Room: {c1}")
-print(f"Number of Superior Room: {c2}")
-print(f"Number of Executive Rooms: {c3}")
-print(f"Number of Presidential: {c4}")
+    cr = obj.cursor()
+
+    #import this from the sql file
+    #better to import each sperately, else a list
+    #sum of the worker salaries
+    cr.execute("select sum(count) from rooms where roomno between 1 and 40")
+    a = cr.fetchone()
+    print(f"Number of Standard Room: {a[0]}")
+
+    cr.execute("select sum(count) from rooms where roomno between 41 and 60")
+    b = cr.fetchone()
+    print(f"Number of Superior Room: {b[0]}")
+
+    cr.execute("select sum(count) from rooms where roomno between 61 and 70")
+    c = cr.fetchone()
+    print(f"Number of Executive Rooms: {c[0]}")
+
+    cr.execute("select sum(count) from rooms where roomno between 61 and 70")
+    d = cr.fetchone()
+    print(f"Number of Presidential: {d[0]}")
 
 
-colors = ["red", "orange", "yellow", "lightskyblue"]
+    colors = ["red", "orange", "yellow", "lightskyblue"]
 
 
-mpl.pie([c1, c2, c3, c4], labels = ["Standard Rooms", "Superior Rooms", "Executive Rooms", "Presidential Suites"], colors = colors)
+    mpl.pie([a, b, c, d], labels = ["Standard Rooms", "Superior Rooms", "Executive Rooms", "Presidential Suites"], colors = colors)
 
-mpl.show()
+    mpl.show()
