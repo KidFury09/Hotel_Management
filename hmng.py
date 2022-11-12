424a7b49242e78e590c39e1db3df3e395ff08e30
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-12 08:37:54 +0400

    Update hmng.py

---
+++ hmng.py
@@ -0,0 +1,26 @@
+from Earnings import earning
+from Employees import employee
+
+def manager():
+    while True:
+        print("""
+        Choose an option
+        a) Employee Fuctions
+        b) Room records
+        c) Exit
+        """)
+
+        cho = input("Enter the option here: ").lower()
+
+        if cho == "a":
+            employee()
+        
+        elif cho == "b":
+            earning()
+
+        elif cho == "c":
+            break
+
+        else:
+            print()
+            print("Invalid Input")
