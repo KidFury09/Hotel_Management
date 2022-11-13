2720c5dd9aba2e25544caaebbc0bfaf7b7b61e77
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-13 11:51:28 +0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -8,12 +8,16 @@ print()
 User = input("Enter employee name: ")
 Pass = input("Enter password: ")
 print()
+c = 0
 
 while True:
     if User in ids:
         psw = ids[User]
         if Pass == psw[0] and psw[1] == "Manager":
             manager()
+            c = 1
+            if c == 1:
+                break
         elif Pass == psw[0] and psw[1] == "Front_Desk":
             pass
             #check for which user; manager or front desk
