2439abca434dee9bb4f849cc28966dd93f8025fe
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-16 21:58:05 +0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,5 +1,6 @@
 from FrontDesk import desk
 from hmng import manager
+from time import sleep
 
 ids = {"Keshav" : ["Password","Manager"], "Hayden" : ["password", "Front_Desk"]}
 #Assume Keshav is the manager and Hayden is the front desk clerk
@@ -14,11 +15,13 @@ c = 0
 while True:
     if User in ids:
         psw = ids[User]
+
         if Pass == psw[0] and psw[1] == "Manager":
             manager()
             c = 1
             if c == 1:
                 break
+        
         elif Pass == psw[0] and psw[1] == "Front_Desk":
             desk()
             c = 1
@@ -31,9 +34,12 @@ while True:
             User = input("Enter employee name: ")
             Pass = input("Enter password: ")
             print()
+            sleep(2)
+
     else:
         print("Name is incorrect. Please try again")
         print()
         User = input("Enter employee name: ")
         Pass = input("Enter password: ")
         print()
+        sleep(2)
