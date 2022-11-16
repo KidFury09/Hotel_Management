e2ff2e34e18df991f4711f18d106f133d6dd7752
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-11-16 21:56:45 +0400

    Update FrontDesk.py

--- FrontDesk.py
+++ FrontDesk.py
@@ -1,4 +1,5 @@
 import mysql.connector as mql
+from time import sleep
 
 obj = mql.connect(host = "localhost", user = "root", passwd = "hayden", database = "hotel")
 
@@ -15,12 +16,16 @@ def room_count():
     x = cr.fetchall()
 
     for i in x:
+
         if i[0] in range(1,41) and i[1] == None: # 40 rooms
             c1+=1                
+        
         elif i[0]  in range(41, 61) and i[1] == None: #20 rooms
             c2+=1
+        
         elif i[0] in range(61,71) and i[1] == None: #10
             c3+=1
+        
         else:   
             c4+=1 #5 rooms
     print(f'''Avaliable rooms
@@ -33,6 +38,7 @@ def room_count():
 def desk():
 
     while True:
+        
         print()
         print("""Please Choose An Option
     1) Adding Guest
@@ -42,110 +48,139 @@ def desk():
 
         cho = input("Enter your choice here: ")
         print()
+
         if cho == "1":
             while True:
                 guest = input("Customer name: " )
                 pnum = input("Phone number: ")
                 print()
+                sleep(2)
             
                 false = input("Enter Y to confirm details: ")
 
                 if false.lower() == "y":
                     break
+        
                 else:
                     print("Renter data")
-                    print()
+                    print()   
+        
             print()
             
             room_count()
+            
             while True:
+            
                 print()
                 print("Enter the room type below: ")
                 print()
+            
                 r1 = input("Standard Room(s): ")
                 r2 = input("Superior Room(s): ")
                 r3 = input("Executive Room(s): ")
                 r4 = input("Presidential Suite(s): ")
+            
                 print()
+            
                 false = input("Enter Y to confirm details: ")
+                sleep(2)
 
                 if false.lower() == "y":
                     break
+            
                 else:
                     print("Renter data")
                     print()
+            
             print()
 
             room_l = [r1, r2 ,r3 ,r4]
             room_l2 = []
+            
             for i in room_l:
+            
                 if i == '':
                     room_l2.append(0)
+            
                 else:
                     room_l2.append(int(i))
+            
             print()
+            
             cost = (room_l2[0] * 500) + (room_l2[1] * 650) + (room_l2[2] * 800) + (room_l2[3] * 1000)
             print(f"Total Cost: {cost}")
             print()
+            
             money_g = int(input("Money Given: "))
             change = print(f"Change: {money_g - cost}")
             print("Updated Data")
             print()
-            #create a list that contains [name, [room types], phone number]
-            #put the cost in the sql file as well so its easy to add
-            #store the amount of times each room is used
-            for i in range(4): # [1,2,3,4]
+
+            for i in range(4):
+
                 a = 0
                 b = 0
                 c = 0
                 d = 0
 
                 if i == 0:
+                    
                     while a < room_l2[i]:
+                    
                         cr.execute("select min(roomno) from rooms where guest is null and roomno between 1 and 40")
                         x = cr.fetchone()
                         x = int(x[0])
 
                         cr.execute(f"update rooms set guest = '{guest}', phonenum = {pnum} where roomno = {x}")
                         cr.execute(f"update rooms set count = count + 1 where roomno = {x}")
+                    
                         a += 1
                         obj.commit()
                 
                 elif i == 1:
+                    
                     while b < room_l2[i]:
+                    
                         cr.execute("select min(roomno)from rooms where guest is null and roomno between 41 and 60 ")
                         x = cr.fetchone()
                         x = int(x[0])
 
                         cr.execute(f"update rooms set guest = '{guest}', phonenum = {pnum} where roomno = {x}")
                         cr.execute(f"update rooms set count = count + 1 where roomno = {x}")
+                    
                         b += 1
                         obj.commit()
 
                 elif i == 2:
+                    
                     while c < room_l2[i]:
+                    
                         cr.execute("select min(roomno) from rooms where guest is null and roomno between 61 and 70")
                         x = cr.fetchone()
                         x = int(x[0])
 
                         cr.execute(f"update rooms set guest = '{guest}', phonenum = {pnum} where roomno = {x}")
                         cr.execute(f"update rooms set count = count + 1 where roomno = {x}")
+                    
                         c += 1
                         obj.commit()
                 
                 elif i == 3:
+                    
                     while d < room_l2[i]:
+                    
                         cr.execute("select min(roomno) from rooms where guest is Null and roomno between 71 and 75 ")
                         x = cr.fetchone()
                         x = int(x[0])
 
                         cr.execute(f"update rooms set guest = '{guest}', phonenum = {pnum} where roomno = {x}")
                         cr.execute(f"update rooms set count = count + 1 where roomno = {x}")
+                    
                         d += 1
                         obj.commit()
-            #To add the billing and stuff as well
 
         elif cho == "2":
+            
             n = input("Enter the guest name to be removed: ")
             cr.execute(f"update rooms set guest = Null,  phonenum = Null where guest = '{n}' ")
             obj.commit()
@@ -159,3 +194,4 @@ def desk():
             print()
             print("Invalid Input. Only enter '1' or '2'")
             print()
+            sleep(2)
